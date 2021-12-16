e5fc59f0bbb1cef7e09dfdc27c9ee71ce34a577f
robinleruth <robin.leruth@gmail.com>
2021-12-17 00:09:20 +0100

    second commit

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ ui_queues: List[Queue] = []
 ui_out_queue = Queue()
 
 
+# ================= EVENT =================
+
 class Event(metaclass=abc.ABCMeta):
     @staticmethod
     @abc.abstractmethod
@@ -22,6 +24,21 @@ class RandomSubscriberEvent(Event):
     @staticmethod
     def get_repr():
         return 'RANDOM_SUBSCRIBER'
+
+
+class StrFromUi(Event):
+    def __init__(self, message: str) -> None:
+        self.message = message
+
+    @staticmethod
+    def get_repr():
+        return 'STR_FROM_UI'
+
+    def __repr__(self):
+        return self.message
+
+
+# ================= WORKERS =================
 
 
 class Worker(metaclass=abc.ABCMeta):
@@ -54,7 +71,7 @@ class RandomSubscriber(Worker):
     async def _get_from_source(self):
         while True:
             await self.tell('Ok')
-            await asyncio.sleep(1)
+            await asyncio.sleep(3)
 
     async def start(self):
         loop = asyncio.get_event_loop()
@@ -68,16 +85,28 @@ class RandomSubscriber(Worker):
         return message
 
 
-workers: List[Worker] = [RandomSubscriber(ui_queues)]
+class StrFromUiWorker(Worker):
+    def get_type(self) -> Type[Event]:
+        return StrFromUi
+
+    async def _process_message(self, message) -> Any:
+        return message
+
+
+# ================= SET UP =================
+
+workers: List[Worker] = [RandomSubscriber(ui_queues), StrFromUiWorker(ui_queues)]
 workers_by_type: Dict[str, List[Worker]] = dict()
 
 
 async def init_workers():
+    lst = []
     for w in workers:
-        await w.start()
+        lst.append(w)
         if w.get_type() not in workers_by_type:
             workers_by_type[w.get_type().get_repr()] = []
         workers_by_type[w.get_type().get_repr()].append(w)
+    await asyncio.gather(*[w.start() for w in lst])
 
 dispatcher_queue = asyncio.Queue()
 
@@ -101,7 +130,7 @@ async def dispatcher():
             for w in workers:
                 await w.tell(message)
         else:
-            print('No worker for type {}', type(message))
+            print(f'No worker for type {message.get_repr()}')
 
 
 def process_message_from_ui():
@@ -120,10 +149,83 @@ get_message_thread.daemon = True
 get_message_thread.start()
 
 
+# ================= UI =================
+
+class NavBar(Frame):
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+        self.parent = parent
+        Label(self, text='Navbar').pack()
+        self.var = IntVar()
+        Radiobutton(self, text='test1', variable=self.var, value=1, command=self.test).pack()
+        Radiobutton(self, text='test2', variable=self.var, value=2, command=self.test).pack()
+
+    def test(self):
+        self.parent.statusbar.set(self.var.get())
+
+
+class ToolBar(Frame):
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+        Label(self, text='Toolbar').pack()
+        Button(self, text='Send event', command=self.send_event).pack()
+
+    def send_event(self):
+        msg = StrFromUi("test from one window")
+        ui_out_queue.put(msg)
+
+
+class StatusBar(Frame):
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+        self.var = StringVar(value='Launching...')
+        self.label = Label(self, textvariable=self.var)
+        self.label.pack()
+
+    def set(self, value):
+        self.var.set(str(value))
+
+
+class MainContent(Frame):
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+        Label(self, text='Main content').pack()
+
+
+class Main(Frame):
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+        self.statusbar = StatusBar(self)
+        self.toolbar = ToolBar(self)
+        self.navbar = NavBar(self)
+        self.main_content = MainContent(self)
+
+        self.statusbar.pack(side='bottom', fill='x')
+        self.toolbar.pack(side='top', fill='x')
+        self.navbar.pack(side='left', fill='y')
+        self.main_content.pack(side='right', fill='both', expand=True)
+
+        self.master.after(100, self.process_queue)
+
+    def process_queue(self):
+        while not self.master.queue.empty():
+            message = self.master.queue.get()
+            if type(message) == str:
+                Label(self.main_content, text=message).pack()
+            elif type(message) == StrFromUi:
+                Label(self.main_content, text=message.message).pack()
+        self.master.after(100, self.process_queue)
+
+
 class App(Tk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.queue = Queue()
+        ui_queues.append(self.queue)
+        self.geometry('300x300')
 
 
 if __name__ == '__main__':
-    App().mainloop()
+    r = App()
+    Main(r).pack(side='top', fill='both', expand=True)
+    r.mainloop()
