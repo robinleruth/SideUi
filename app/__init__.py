d1b9797c0581864f3390fa47be132df7667891b9
robinleruth <robin.leruth@gmail.com>
2022-04-19 11:10:44 +0800

    imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
 from .app import Event, Worker, WorkerMeta, MyFrame, ui_out_queue, VerticalScrolledFrame, metaclass_resolver, launch_ui, \
-    process_message_from_ui, workers
+    process_message_from_ui, workers, Subscriber, FileSubscriber
 
 global workers
