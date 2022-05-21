d91fdcc3ada95351ecaf17869934786f6e38ed46
robinleruth <robin.leruth@gmail.com>
2022-05-21 15:11:13 +0800

    tv sort

--- plugins/tree_view.py
+++ plugins/tree_view.py
@@ -1,3 +1,5 @@
+import datetime as dt
+from functools import partial
 from tkinter import *
 from tkinter.ttk import *
 from typing import List, Type
@@ -47,7 +49,7 @@ class F(MyFrame):
         container.pack(expand=True, fill='both')
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
-        tree = Treeview(container)
+        tree = MyTreeview(container)
         tree.grid(row=0, column=0, sticky='nsew')
 
         def item_selected(t):
@@ -123,7 +125,7 @@ class F(MyFrame):
         tree['show'] = 'headings'
         for i in df.columns:
             tree.column(i, anchor="w", stretch=True, width=10)
-            tree.heading(i, text=i, anchor="w")
+            tree.heading(i, text=i, anchor="w", sort_by='num')
         for index, row in df.iterrows():
             tree.insert("", END, text=index, values=list(row))
 
@@ -162,3 +164,74 @@ class EntryPopup(Entry):
 
         # returns 'break' to interrupt default key-bindings
         return 'break'
+
+
+class MyTreeview(Treeview):
+    def heading(self, column, sort_by=None, **kwargs):
+        if sort_by and not hasattr(kwargs, 'command'):
+            func = getattr(self, f"_sort_by_{sort_by}", None)
+            if func:
+                kwargs['command'] = partial(func, column, False)
+            # End of if
+        # End of if
+        return super().heading(column, **kwargs)
+
+    # End of heading()
+
+    def _sort(self, column, reverse, data_type, callback):
+        l = [(self.set(k, column), k) for k in self.get_children('')]
+        l.sort(key=lambda t: data_type(t[0]), reverse=reverse)
+        for index, (_, k) in enumerate(l):
+            self.move(k, '', index)
+        # End of for loop
+        self.heading(column, command=partial(callback, column, not reverse))
+
+    # End of _sort()
+
+    def _sort_by_num(self, column, reverse):
+        self._sort(column, reverse, int, self._sort_by_num)
+
+    # End of _sort_by_num()
+
+    def _sort_by_name(self, column, reverse):
+        self._sort(column, reverse, str, self._sort_by_name)
+
+    # End of _sort_by_num()
+
+    def _sort_by_date(self, column, reverse):
+        def _str_to_datetime(string):
+            return dt.datetime.strptime(string, "%Y-%m-%d")
+
+        # End of _str_to_datetime()
+
+        self._sort(column, reverse, _str_to_datetime, self._sort_by_date)
+
+    # End of _sort_by_num()
+
+    def _sort_by_multidecimal(self, column, reverse):
+        def _multidecimal_to_str(string):
+            arrString = string.split(".")
+            strNum = ""
+            for iValue in arrString:
+                strValue = f"{int(iValue):02}"
+                strNum = "".join([strNum, str(strValue)])
+            # End of for loop
+            strNum = "".join([strNum, "0000000"])
+            return int(strNum[:8])
+
+        # End of _multidecimal_to_str()
+
+        self._sort(column, reverse, _multidecimal_to_str, self._sort_by_multidecimal)
+
+    # End of _sort_by_num()
+
+    def _sort_by_numcomma(self, column, reverse):
+        def _numcomma_to_num(string):
+            return int(string.replace(",", ""))
+
+        # End of _numcomma_to_num()
+
+        self._sort(column, reverse, _numcomma_to_num, self._sort_by_numcomma)
+    # End of _sort_by_num()
+
+# End of class MyTreeview
