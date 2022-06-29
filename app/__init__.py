f873d104d6231941de2fa5ae5209f14a78377830
robinleruth <robin.leruth@gmail.com>
2022-06-29 17:19:26 +0800

    done

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from .app import Event, Worker, WorkerMeta, MyFrame, ui_out_queue, VerticalScrolledFrame, metaclass_resolver, launch_ui, \
-    process_message_from_ui, workers, Subscriber, FileSubscriber
+from .mini_app import Event, Worker, WorkerMeta, MyFrame, ui_out_queue, VerticalScrolledFrame, metaclass_resolver, launch_ui, \
+    process_message_from_ui, workers, Subscriber, FileSubscriber, open_file
 
 global workers
